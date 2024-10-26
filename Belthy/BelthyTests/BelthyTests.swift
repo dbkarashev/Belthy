f6182b254cfd1b37366cef13af8d2a69e8a346a6
dbkarashev <dbkarashev@gmail.com>
2024-10-26 20:15:29 +0700

    Initial project setup

---
+++ Belthy/BelthyTests/BelthyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BelthyTests.swift
+//  BelthyTests
+//
+//  Created by Damir Karashev on 26.10.2024.
+//
+
+import Testing
+@testable import Belthy
+
+struct BelthyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
